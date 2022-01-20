d3bb545d933a57243285296883b7139881d317e9
Sinwoo Bang <sinwoobang@gmail.com>
2022-01-20 13:59:07 -0500

    Improve input UX

--- index_constraint_migration.mjs
+++ index_constraint_migration.mjs
@@ -1,17 +1,27 @@
 #!/usr/bin/env zx
 
-console.log('[Input DB Information]');
+const log = console.log
 
-let srcDBHost = await question('Source DB Host: ');
-let srcDBUser = await question('Source DB User: ');
-let srcDBName = await question('Source DB Name: ');
+log();
+log(chalk.green("Welcome to dms-psql-post-data"));
+log();
 
-let tarDBHost = await question('Target DB Host: ');
-let tarDBUser = await question('Target DB User: ');
-let tarDBName = await question('Target DB Name: ');
+log(chalk.yellow('Input your Source DB info'));
 
-console.log('[LOG] Dumping Source DB Post-data(Index, Constraint)...');
+let srcDBHost = await question('[1/8] Source DB Host: ');
+let srcDBUser = await question('[2/8] Source DB User: ');
+let srcDBName = await question('[3/8] Source DB Name: ');
+let srcDBPass = await question('[4/8] Source DB Password: ');
+
+let tarDBHost = await question('[5/8] Target DB Host: ');
+let tarDBUser = await question('[6/8] Target DB User: ');
+let tarDBName = await question('[7/8] Target DB Name: ');
+let tarDBPass = await question('[8/8] Target DB Password: ');
+
+process.env.PGPASSWORD = srcDBPass
+log(chalk.yellow('Dumping Source DB Post-data(Index, Constraint)...'));
 await $`pg_dump -h ${srcDBHost} -U ${srcDBUser} -Fc ${srcDBName} --section=post-data > postdata.dump`
 
-console.log('[LOG] Restoring Post-data on Target DB...');
-await $`pg_restore -h ${tarDBHost} -U ${tarDBUser} -d ${tarDBName} -v postdata.dump`
+process.env.PGPASSWORD = tarDBPass
+log(chalk.yellow('Restoring Post-data on Target DB...'));
+await $`pg_restore -h ${tarDBHost} -U ${tarDBUser} -d ${tarDBName} -j 10 -v postdata.dump`
